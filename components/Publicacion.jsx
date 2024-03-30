87f15be922b5a1b34873690f83b9f85248f00a6b
Facu Sanchez <facusanchz96@gmail.com>
2024-03-30 20:31:33 -0300

    tienda

--- components/Publicacion.jsx
+++ components/Publicacion.jsx
@@ -18,6 +18,7 @@ const mg=  Math.floor(Math.random() * 900) + 100;
 <div className=' flex'>
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyjdMBZVclfCempeD5wO1rWd_m8KkkorVMhil6DmW-nPv6XhKAgCfPC5cUfOLqJ8jaCq0&usqp=CAU" className='w-7 mr-4 ' alt="" />
 <img src="https://cdn.icon-icons.com/icons2/1875/PNG/512/comment_120216.png" className=' w-9 ' alt="" />
+<img src="https://e7.pngegg.com/pngimages/625/782/png-clipart-send-icon-x-plane-computer-mouse-aircraft-airplane-pointer-mouse-cursor-miscellaneous-angle-thumbnail.png" className=' w-8 ml-2 ' alt="" />
 <img src="https://cdn-icons-png.flaticon.com/512/5662/5662990.png" className=' w-8 ml-auto' alt="" />
 </div>
 <br />
